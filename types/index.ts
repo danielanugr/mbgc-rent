54a47b8cbfd49919b89cc63c02b5dc29e9c00f28
Daniel <danielanugr@yahoo.co.id>
2025-03-27 13:03:26 +0800

    add serverless env config for prisma neon
    Signed-off-by: Daniel <danielanugr@yahoo.co.id>

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,8 @@
 import { z } from 'zod';
 import { insertProductSchema } from '@/lib/validator';
-import { Decimal } from '@prisma/client/runtime/library';
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
-  rating: Decimal;
+  rating: string;
   createdAt: Date;
 };
