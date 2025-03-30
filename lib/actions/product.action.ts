a9e8d61e06fb405f78f204c7abec9325e17c0c3b
Daniel <danielanugr@yahoo.co.id>
2025-03-30 17:19:13 +0800

    add product page
    Signed-off-by: Daniel <danielanugr@yahoo.co.id>

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -12,3 +12,12 @@ export async function getLatestProducts() {
 
   return convertToPlainObject(data);
 }
+
+//get single product by slug
+export async function getProductBySlug(slug: string) {
+  const data = await prisma.product.findFirst({
+    where: { slug },
+  });
+
+  return convertToPlainObject(data);
+}
